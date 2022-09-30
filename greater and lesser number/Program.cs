5fd6fc2d7db29b34b5895f2cd20a6c47a9f10db1
Oleg Smile <valsov-9988@mail.ru>
2022-09-30 23:28:48 +0300

    added Task number 2

---
+++ greater and lesser number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, enter the first number.");
+int number1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Hello, enter the first number.");
+int number2 = int.Parse(Console.ReadLine() ?? "");
+
+if (number1 > number2)
+{
+    Console.WriteLine("a = " + number1 + "; b = " + number2 + "-> max = " + number1);
+}
+else
+    Console.WriteLine("a = " + number1 + "; b = " + number2 + " -> max = " + number2);
