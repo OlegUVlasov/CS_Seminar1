95ba84bfdde5896f845fced9931bbf7cb62228d0
Oleg Smile <valsov-9988@mail.ru>
2022-10-01 00:04:45 +0300

    added Task number 6

---
+++ even number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, enter the number.");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if (number % 2 == 1)
+{
+    Console.WriteLine(number  + " -> нет, числло не чётное");
+}
+else
+    Console.WriteLine(number  + " -> да, числло чётное");
+
