f4efd97b3c0b03ad3d7404f2c5e29d3862921ffa
Oleg Smile <valsov-9988@mail.ru>
2022-10-01 00:33:36 +0300

    added Task number 8

---
+++ even numbers from 1 to N/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, enter the number.");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int count = 2;
+
+Console.Write(number + " ->");
+
+while (count <= number)
+{
+    if (count == number || count > number)
+    {
+        Console.Write(" " + count);
+    }
+    else
+        Console.Write(" " + count + ",");
+    count += 2;
+}
